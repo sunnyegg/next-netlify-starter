8983c01dacf450fb32d72a9bdafb2cbf7ec05312
sunnyegg <adilatresna21@gmail.com>
2024-03-10 09:20:02 +0700

    edit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>sunnyegg</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Hi!" />
+        <p className="description">Nothing to see here, currently.</p>
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
